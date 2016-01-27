b6c6f8ed761ff7429adca07cf8746b734543e325
Teije van Sloten <teije.van.sloten@hva.nl>
2016-01-27 19:58:31 +0100

    Qr scan added

--- app/src/main/java/com/example/tb_laota/BestDeals/MainActivity.java
+++ app/src/main/java/com/example/tb_laota/BestDeals/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.tb_laota.BestDeals;
 
 import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
@@ -116,7 +119,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         AppController.getmInstance().addToRequesQueue(jsonArrayRequest);
+
+        ImageButton qrButton = (ImageButton) findViewById(R.id.scanQrCodeButton);
+        qrButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+
+                    startActivityForResult(intent, 0);
+                } catch (Exception e) {
+
+                    Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+                    Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+                    startActivity(marketIntent);
+
+                }
+            }
+        });
     }
+
     public void hideDialog(){
         if(dialog !=null){
             dialog.dismiss();
@@ -128,6 +151,20 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                Toast.makeText(MainActivity.this, contents, Toast.LENGTH_LONG);
+            }
+            if(resultCode == RESULT_CANCELED){
+                //handle cancel
+            }
+        }
     }
 
     @Override
